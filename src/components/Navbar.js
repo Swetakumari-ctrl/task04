6738603a8666a92d6c528d4fdc4c092fbe96d55a
SooditK <sooditkumarabc@gmail.com>
2022-03-18 00:14:56 +0530

    feat: Added Search Functionality w/ Product Arraay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Card, CardGroup, Form, FormControl, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const productdata = [
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2",
+    shopname: "SHOE NAME 11",
+    rating: "5.0",
+    price: "2,350.00",
+    deliveryby: "Tomorrow",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2",
+    shopname: "SHOE NAME 22",
+    rating: "5.0",
+    price: "1,350.00",
+    deliveryby: "Friday, 6 Feb",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2",
+    shopname: "SHOE NAME 43",
+    rating: "4.0",
+    price: "4,550.00",
+    deliveryby: "Tomorrow",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2",
+    shopname: "SHOE NAME 48",
+    rating: "3.0",
+    price: "9,950.00",
+    deliveryby: "Tomorrow",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2",
+    shopname: "SHOE NAME 52",
+    rating: "2.0",
+    price: "350.00",
+    deliveryby: "Thursday, Feb 5",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2",
+    shopname: "SHOE NAME 13",
+    rating: "3.0",
+    price: "4,650.00",
+    deliveryby: "Monday, Feb 20",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2",
+    shopname: "SHOE NAME 11",
+    rating: "4.0",
+    price: "3,350.00",
+    deliveryby: "Monday, Feb 20",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2",
+    shopname: "SHOE NAME 22",
+    rating: "1.0",
+    price: "1,350.00",
+    deliveryby: "Tuesday, Feb 3",
+  },
+  {
+    imagesrc:
+      "https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2",
+    shopname: "SHOE NAME 43",
+    rating: "2.0",
+    price: "4,550.00",
+    deliveryby: "Tomorrow",
+  },
+];
+
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const [product, setProduct] = useState(productdata);
+  const [filteredproduct, setFilteredProduct] = useState(productdata);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFilteredProduct(
+      product.filter((product) => {
+        return product.shopname.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  };
+
   return (
     <>
       <nav className="main-nav">
@@ -55,8 +147,11 @@ const Navbar = () => {
           placeholder="Search"
           className="me-2"
           aria-label="Search"
+          onChange={(e) => handleChange(e)}
         />
-        <Button variant="warning">SEARCH</Button>{" "}
+        <Button onClick={(e) => handleSubmit(e)} variant="warning">
+          SEARCH
+        </Button>{" "}
       </Form>
       <div className="products">
         <p>
@@ -64,208 +159,49 @@ const Navbar = () => {
         </p>
       </div>
       <CardGroup className="cardbox">
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>SHOP NAME 11</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 5.0</Button>{" "}
+        {filteredproduct.map((prod) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            >
+              <div
+                style={{
+                  width: "26rem",
+                  boxShadow:
+                    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                  margin: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <Card.Img variant="top" src={prod.imagesrc} />
+                <Card.Body className="box">
+                  <Card.Title>
+                    <h1>
+                      <b>{prod.shopname}</b>
+                    </h1>
+                    <div className="ratings">
+                      <Button variant="warning">★ {prod.rating}</Button>{" "}
+                    </div>
+                  </Card.Title>
+                  <Card.Text>
+                    <p>
+                      <b>₹{prod.price}</b>
+                    </p>
+                  </Card.Text>
+                </Card.Body>
+                <small className="text-muted">
+                  Delivery by: {prod.deliveryby}
+                </small>
               </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹2,350.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Tomorrow</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 22</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 5.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹1,350.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Friday, 6 Feb</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 43</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 4.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹4,550.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Tomorrow</small>
-        </Card>
-      </CardGroup>
-      <CardGroup className="cardbox">
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 48</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 3.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹9,950.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Tomorrow</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 52</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 2.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹350.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Thursday, Feb 5</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 13</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 3.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹4,650.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Monday, Feb 20</small>
-        </Card>
-      </CardGroup>
-      <CardGroup className="cardbox">
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/68185/8996/1122017-TEBC_4__20503.1618516034.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 11</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 4.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹2,350.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Monday, Feb 20</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/63320/8698/(1110518-WDDS)_(4)__02007.1616607135.png?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 22</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 1.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹1,350.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Tuesday, Feb 3</small>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://cdn11.bigcommerce.com/s-6wbwpjejja/images/stencil/1280x1280/products/69053/8974/1119394-AEBL_4__83772.1618514248.jpg?c=2"
-          />
-          <Card.Body className="box">
-            <Card.Title>
-              <h1>
-                <b>Shoe Name 43</b>
-              </h1>
-              <div className="ratings">
-                <Button variant="warning">★ 2.0</Button>{" "}
-              </div>
-            </Card.Title>
-            <Card.Text>
-              <p>
-                <b>₹4,550.00</b>
-              </p>
-            </Card.Text>
-          </Card.Body>
-          <small className="text-muted">Delivery by: Tomorrow</small>
-        </Card>
+            </div>
+          );
+        })}
       </CardGroup>
     </>
   );
